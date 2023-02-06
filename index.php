3274985f6b81a526d6aa037be18fec2edb066a8a
jaromeaviles <jaromeaviles@gmail.com>
2023-02-06 10:36:16 +0800

    added breakpoint setup and added hero text

--- index.php
+++ index.php
@@ -6,8 +6,15 @@
   <div class="container-fluid">
       <div class="row">
           <div class="col-lg-12 p-0">
-            <div class="hero-banner">
-              <img src="dist/images/hero-banner.jpg" alt="hero-banner">
+            <div class="hero-banner-container">
+              <div class="hero-banner">
+                <img src="dist/images/hero-banner.jpg" alt="hero-banner">
+              </div>
+              <div class="hero-text">
+                  <h1 class="color-white">kidoos <br /> Kindergarten</h1>
+                  <p class="color-white">Teaching future leaders</p>
+                  <button class="btn btn-primary custom-button">Learn More</button>
+              </div>
             </div>
           </div>
       </div>
