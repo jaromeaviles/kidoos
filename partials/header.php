c944d2a379371750a3282d80cb126e8b0da15bd7
jaromeaviles <jaromeaviles@gmail.com>
2023-02-08 11:52:52 +0800

    added student page, display records

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,9 @@
+<!-- Connections -->
+
+<?php
+  require 'connections/config.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,20 +32,20 @@
 
 <nav class="navbar navbar-expand-lg">
   <div class="container">
-    <a class="navbar-brand font-playfair-bold color-white" href="#">kidoos</a>
+    <a class="navbar-brand font-playfair-bold color-white" href="index.php">kidoos</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link color-white" href="#">Home</a>
+          <a class="nav-link color-white" href="index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link color-white" href="#">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link color-white" href="#">Students</a>
+          <a class="nav-link color-white" href="students.php">Students</a>
         </li>
         <li class="nav-item">
           <a class="nav-link color-white" href="#">Login</a>
---
+++ students.php
@@ -0,0 +1,56 @@
+<?php 
+    require_once 'partials/header.php';
+
+    $sql = 'SELECT * FROM students';
+
+    $results = $conn->query($sql) or die($conn->error);
+
+    $row = $results->fetch_assoc();
+?>
+
+<main class="sub-pages">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="inner-hero">
+                <img src="dist/images/reading.jpg" alt="bonding">
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- table -->
+
+<div class="container student-record">
+   <div class="row">
+      <table>
+         <thead>
+            <tr>
+               <th scope="col">Full Name</th>
+               <th scope="col">Last Name</th>
+               <th scope="col">Email</th>
+               <th scope="col">Gender</th>
+            </tr>
+         </thead>
+         <tbody>
+            <?php do { ?>
+            <tr>
+               <td data-label="Full Name"><?php echo $row['full_name']; ?></td>
+               <td data-label="Last Name"><?php echo $row['last_name']; ?></td>
+               <td data-label="Email"><?php echo $row['email']; ?></td>
+               <td data-label="Gender"><?php echo $row['gender']; ?></td>
+            </tr>
+            <?php } while($row = $results->fetch_assoc()); ?>
+         </tbody>
+      </table>
+   </div>
+</div>
+</div>
+
+
+
+</main>
+
+<?php
+    require_once 'partials/footer.php';
+?>
